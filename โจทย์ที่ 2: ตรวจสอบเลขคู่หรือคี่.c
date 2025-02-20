1b508a23b6a2d466b068e5c35c67e92209a4b979
DocTeeraThxp <106339662+Teera235@users.noreply.github.com>
2025-02-20 13:43:19 +0700

    Update โจทย์ที่ 2: ตรวจสอบเลขคู่หรือคี่.c

--- โจทย์ที่ 2: ตรวจสอบเลขคู่หรือคี่.c
+++ โจทย์ที่ 2: ตรวจสอบเลขคู่หรือคี่.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main() {
-    // Task 2: Check if a number is odd or even
-    // Receive 1 integer, calculate modulo 2, and print "Even" or "Odd"
+
     int num;
     printf("Enter a number: ");
     scanf("%d", &num);
